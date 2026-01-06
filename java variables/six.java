559224ac8178c1a82baf5e55e607755bc5418cb7
sandeepraj543 <sandeepkb805124@gmail.com>
2026-01-06 21:52:43 +0530

    add six variable question

---
+++ java variables/six.java
@@ -0,0 +1,8 @@
+public class six {
+    public static void main(String[] args){
+        // float c = 25.0f ;
+        float c = 25 ;
+        float F = ( c * 9/5 ) + 32 ;
+        System.out.println("fahrenheit value is:" + F );
+    }
+}
