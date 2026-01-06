117b85a6be56c3349ab87d60700d21a6a3349abf
sandeepraj543 <sandeepkb805124@gmail.com>
2026-01-06 22:26:37 +0530

    add seven program

---
+++ java variables/seven.java
@@ -0,0 +1,14 @@
+public class seven {
+    public static void main(String[] args) {
+        int first_no = 20;
+        int second_no = 30; 
+
+        first_no = first_no + second_no ;
+        second_no = first_no - second_no ;
+        first_no = first_no - second_no ;
+
+        System.out.println("first no is:" + first_no);
+        System.out.println("second no is:" + second_no);
+        
+    }
+}
